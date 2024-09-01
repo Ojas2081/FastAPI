a0fe88f609c19326d8bd76c46e0e59842ce24c67
Ojas <ojasacharekar257@gmail.com>
2024-09-01 20:10:10 +0530

    added new db creds

--- database.py
+++ database.py
@@ -5,7 +5,9 @@ from sqlalchemy.orm import sessionmaker
 
 # SQL_ALCHEMY_DATABASE_URL = '''sqlite:///./todos.db'''
 # SQL_ALCHEMY_DATABASE_URL = '''postgresql://postgres:ojas1234@localhost/TodoApplicationDatabase'''
-SQL_ALCHEMY_DATABASE_URL = '''postgresql://fidato:oqurEydTUjt4vZIBhqIYmrmNGqv6ihEk@dpg-cqkqlbt6l47c73et83v0-a.oregon-postgres.render.com/todoapp_db_8bzl'''
+# SQL_ALCHEMY_DATABASE_URL = '''postgresql://fidato:oqurEydTUjt4vZIBhqIYmrmNGqv6ihEk@dpg-cqkqlbt6l47c73et83v0-a.oregon-postgres.render.com/todoapp_db_8bzl'''
+SQL_ALCHEMY_DATABASE_URL = '''postgresql://fidato_1:6eMBjMnlScvIig2iQb3HsrnJzxNhqLme@dpg-cra7ni56l47c73d94ic0-a.oregon-postgres.render.com/todoapp_db_2'''
+
 
 engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
 
